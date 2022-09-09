98cc15492456809ae9ad78b00dd732a1b218860f
Tian <liu00839@umn.edu>
2022-09-08 23:04:58 -0500

    change time display

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,8 +3,8 @@
         <div class="ui top fixed inverted huge menu">
             <div class="right menu">
                 <a class="active item">Home</a>
-                <a class="item">Nav2</a>
-                <a class="item">Nav3</a>
+                <!-- <a class="item">Nav2</a>
+                <a class="item">Nav3</a> -->
             </div>
         </div>
     </div>
